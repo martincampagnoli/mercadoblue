67bab953e281613b12067b86fda14eb503caa6e3
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-02-15 10:28:41 +0100

    Add firebase package and config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireModule } from '@angular/fire/compat';
+
+//UPDATE firebase config HERE
+const firebaseConfig = {
+  apiKey: "AIzaSyAa_45DztHO0cemyoECFbBe_Bm_WGS6vV0",
+  authDomain: "stube-2d651.firebaseapp.com",
+  databaseURL: 'https://stube-2d651-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: "stube-2d651",
+  storageBucket: "stube-2d651.appspot.com",
+  messagingSenderId: "40376188203",
+  appId: "1:40376188203:web:808d89da57f2e2fc6b596e"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
