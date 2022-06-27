0f4643b83e2482bd89f90ef576ac40713c0e8256
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-06-27 13:26:56 +0200

    Update project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/whip'),
+      dir: require('path').join(__dirname, './coverage/mercadoblue'),
       subdir: '.',
       reporters: [
         { type: 'html' },
